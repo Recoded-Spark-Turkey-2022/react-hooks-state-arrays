765d7752fb83f039114eff5440ef79264c15aa66
Hadi <hadi.maleh@gmail.com>
2023-01-12 14:41:40 +0300

    completed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,7 +3,7 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
-
+  const [filterBy, setFilterBy] = useState("All");
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
     const freshspicyFood = [...foods, newFood];
@@ -21,15 +21,60 @@ function SpicyFoodList() {
     }
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id} onClick={handleDeleteFood}>
+  function handelDeletall(id) {
+    const index = foods.filter((food) => food.id === id);
+    if (index !== -1) {
+      const deletedFood = foods.slice(index, index);
+      setFoods(deletedFood);
+    }
+  }
+
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+  // function handleHeatInc(id) {
+  //   const newFoodArray = foods.map((food) => {
+  //     if (food.id === id) {
+  //       return {
+  //         heatLevel: food.heatLevel + 1,
+  //       };
+  //     } else {
+  //       return food;
+  //     }
+  //   });
+  //   setFoods(newFoodArray);
+  // }
+  // const foodList = foods.map((food) => (
+  //   <li onClick={handleHeatInc} key={food.id}>
+  //     {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  //   </li>
+  // ));
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleFilterChange(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
-
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
+      <button onClick={handleDeleteFood}>Delete latest Food</button>
+      <button onClick={handelDeletall}>Delete all Food</button>
+
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
       <ul>{foodList}</ul>
     </div>
   );
